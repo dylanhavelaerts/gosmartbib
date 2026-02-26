576299a8d26c042971068048705a78b14cdab035
Talia Journée <talia.journee@hotmail.com>
2026-02-26 11:47:10 +0100

    Add nav bar to dashboard page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,10 @@ import { useEffect, useState } from "react";
 export default function Home() {
   return (
     <><nav>
+      <button>Startpagina</button>
+      <button>Catalogus</button>
+      <button>Mijn uitleningen</button>
+      <button>Mijn leeslijst & favorieten</button>
       </nav>
       <main>
       </main>
