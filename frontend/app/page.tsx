0f749e9d5e07bd5acc291ea1beaff33f61bcf5d4
Talia Journée <talia.journee@hotmail.com>
2026-02-26 13:09:33 +0100

    Add searchbar and cataloguebutton

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,13 @@ export default function Home() {
   return (
     <>
       <main>
+        <div id="searchBox">
+          <div className="searchbar" >         
+            <input type="text" placeholder="Titel, auteur, genre, onderwerp"/>
+            <button id="searchButton">🔎︎</button>
+          </div>
+          <button className="semitransparentButton">Bekijk Catalogus →</button>
+        </div>
       </main>
       <footer>
       </footer></>
