0c1e3d03764a7c65a14308076befe43410f57e48
Talia Journée <talia.journee@hotmail.com>
2026-03-08 17:19:19 +0100

    Add correct routing

--- backend/testBackend/src/main/java/edu/ap/testbackend/controllers/BookController.java
+++ backend/testBackend/src/main/java/edu/ap/testbackend/controllers/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/books")
+@RequestMapping("/books")
 @CrossOrigin(origins = "*")
 public class BookController {
     private final BookService bookService;
