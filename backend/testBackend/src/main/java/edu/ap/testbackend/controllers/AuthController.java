9b9cea8b004cbc376fb6809c8f7c64fd4f88ea0d
Dylan Havelaerts <dylan.havelaerts@gmail.com>
2026-03-18 14:54:44 +0100

    Authorization Test

--- backend/testBackend/src/main/java/edu/ap/testbackend/controllers/AuthController.java
+++ backend/testBackend/src/main/java/edu/ap/testbackend/controllers/AuthController.java
@@ -1,7 +1,6 @@
 package edu.ap.testbackend.controllers;
 
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -9,23 +8,52 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final String smartschoolAuthorizationUrl;
+    private final String smartschoolClientId;
+    private final String redirectUrl;
+
+    public AuthController(
+            @Value("${spring.security.oauth2.client.provider.smartschool.authorization-uri}") String smartschoolAuthorizationUrl,
+            @Value("${spring.security.oauth2.client.registration.smartschool.client-id}") String smartschoolClientId,
+            @Value("${spring.security.oauth2.client.registration.smartschool.redirect-uri}") String redirectUrl
+    ) {
+        this.smartschoolAuthorizationUrl = smartschoolAuthorizationUrl;
+        this.smartschoolClientId = smartschoolClientId;
+        this.redirectUrl = redirectUrl;
+    }
+
     /**
      * /login endpoint start het OAuth2 login proces, we redirecten de gebruiker naar de Smartschool login pagina.
      * Na een succesvolle login bij Smartschool zal Spring Security automatisch de gebruiker terug redirecten
      * naar de frontend (http://localhost:3000/) of (https://gosmartbib.tech/)
      */
+//    @GetMapping("/login")
+//    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        String authorizationPath = request.getContextPath() + "/oauth2/authorization/smartschool";
+//        response.sendRedirect(authorizationPath);
+//    }
+
+    // Voorbeeld: “/OAuth?client_id=[client_id]&redirect_uri=[redirect_uri]&response_type=code&scope=[scope1 scope2 scope3]”
     @GetMapping("/login")
-    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String authorizationPath = request.getContextPath() + "/oauth2/authorization/smartschool";
-        response.sendRedirect(authorizationPath);
+    public RedirectView login() {
+        String url = UriComponentsBuilder.fromUriString(smartschoolAuthorizationUrl)
+                .queryParam("client_id", smartschoolClientId)
+                .queryParam("redirect_uri", redirectUrl)
+                .queryParam("response_type", "code")
+                .queryParam("scope", "userinfo")
+                .build(true)
+                .toUriString();
+
+        return new RedirectView(url);
     }
 
     /**
@@ -38,7 +66,6 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        // We returnen de naam, email en alle attributes van de gebruiker die we van Smartschool ontvangen
         return ResponseEntity.ok(Map.of(
                 "name", user.getAttribute("name"),
                 "email", user.getAttribute("email"),
