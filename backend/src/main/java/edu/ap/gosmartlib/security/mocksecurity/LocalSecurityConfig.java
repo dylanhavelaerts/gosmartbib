db4859074b395d56eddcf36dd75fd35ee921ca55
Talia Journée <talia.journee@hotmail.com>
2026-03-23 01:09:03 +0100

    Merge main to branch

--- backend/src/main/java/edu/ap/gosmartlib/security/mocksecurity/LocalSecurityConfig.java
+++ backend/src/main/java/edu/ap/gosmartlib/security/mocksecurity/LocalSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,7 +16,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@EnableMethodSecurity
 @Profile("local") // Security config voor lokaal, met mock auth en geen CORS
 @EnableMethodSecurity
 @RequiredArgsConstructor
