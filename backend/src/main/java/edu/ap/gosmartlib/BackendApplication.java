ddff62667032e0b592916025d52a69fbb6ea820e
Dylan Havelaerts <dylanhavelaerts@gmail.com>
2026-05-09 12:03:45 +0200

    Fix conflict issue

--- backend/src/main/java/edu/ap/gosmartlib/BackendApplication.java
+++ backend/src/main/java/edu/ap/gosmartlib/BackendApplication.java
@@ -5,11 +5,7 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-<<<<<<< HEAD
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-=======
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
->>>>>>> 8f458e69877bb36fc3c9706148ae8c6a3dea5210
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -22,29 +18,11 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-/**
-<<<<<<< HEAD
- * Configureer een  timeout voor REST API calls.
- * Dit zorgt ervoor dat als een externe service niet binnen de opgegeven tijd reageert,
- * de call wordt afgebroken en een foutmelding wordt teruggegeven.
-=======
- * Configureer een  timeout voor REST API calls. 
- * Dit zorgt ervoor dat als een externe service niet binnen de opgegeven tijd reageert, 
- * de call wordt afgebroken en een foutmelding wordt teruggegeven. 
->>>>>>> 8f458e69877bb36fc3c9706148ae8c6a3dea5210
- * Dit voorkomt dat je applicatie vastloopt bij het wachten op een reactie van een trage of niet-beschikbare service.
- */
     @Bean
     public RestTemplate restTemplate() {
-<<<<<<< HEAD
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(Duration.ofMillis(5000));
         factory.setReadTimeout(Duration.ofMillis(15000));
-=======
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(15000);
->>>>>>> 8f458e69877bb36fc3c9706148ae8c6a3dea5210
         return new RestTemplate(factory);
     }
 
