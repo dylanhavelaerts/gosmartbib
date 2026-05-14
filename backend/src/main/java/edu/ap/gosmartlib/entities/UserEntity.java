226dab95e04b116a0d4d778748b046617f7a47e7
Dylan Havelaerts <dylan.havelaerts@gmail.com>
2026-05-14 15:36:08 +0200

    Remove deactivation fields and update class props on each login

--- backend/src/main/java/edu/ap/gosmartlib/entities/UserEntity.java
+++ backend/src/main/java/edu/ap/gosmartlib/entities/UserEntity.java
@@ -4,7 +4,6 @@ import edu.ap.gosmartlib.util.UserRoles;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -39,35 +38,9 @@ public class UserEntity {
     @JoinTable(name = "tbl_user_classes", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "class_id"))
     private Set<SchoolClassEntity> classes = new HashSet<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<ReviewEntity> reviews = new ArrayList<>();
-
-    // Soft delete veld
-    @Column(name = "is_active", nullable = false)
-    private boolean active = true;
-
-    // Als een gebruiker gedeactiveerd is,
-    // kunnen we de datum van deactivering en de geplande verwijderdatum bijhouden
-    // Later nog bij de klant horen hoe lang dit moet zijn (bv. 1 jaar na
-    // deactivering)
-    @Column(name = "deactivated_at")
-    private LocalDateTime deactivatedAt;
-
-    @Column(name = "scheduled_deletion_at")
-    private LocalDateTime scheduledDeletionAt;
 
     @Column(name = "oneroster_sourced_id", unique = true)
     private String onerosterSourcedId;
-
-    public void deactivate(long retentionDays) {
-        this.active = false;
-        this.deactivatedAt = LocalDateTime.now();
-        this.scheduledDeletionAt = this.deactivatedAt.plusDays(retentionDays);
-    }
-
-    public void reactivate() {
-        this.active = true;
-        this.deactivatedAt = null;
-        this.scheduledDeletionAt = null;
-    }
 }
